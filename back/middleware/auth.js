7c01b77165547091a860bbeed306aab0f8bef28f
zinebelou <zinebelouaghlidi@gmail.com>
2026-06-04 10:10:09 +0100

    pret

---
+++ back/middleware/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+// Vérifie que l'utilisateur est connecté
+exports.protect = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: 'Non autorisé, token manquant' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select('-password');
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Token invalide' });
+  }
+};
+
+// Vérifie que l'utilisateur est admin
+exports.adminOnly = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    next();
+  } else {
+    res.status(403).json({ message: 'Accès réservé aux administrateurs' });
+  }
+};
